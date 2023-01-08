3acf6eb7125770902f6881b13f7ebbb0b0655f08
tkelbert <85652358+tkelbert@users.noreply.github.com>
2023-01-08 14:01:17 +0000

    Pretty much a working game; half detector for win only detects win if the token is at one of the end points

--- c4.py
+++ c4.py
@@ -1,6 +1,6 @@
 import colorama
 from colorama import Back, Fore, init
-import random
+from random import randint, random
 
 def pct(text, color):
     # Use colorama to set the foreground color of the text
@@ -27,29 +27,6 @@ game = CFB()  # Create a new CFB object
 
 game.print_board()  # Print the board
 
-
-
-
-#def play_game():
-# #board = CFB()
-    #board.play_move(0, 0)
-    #board.print_board()```
-
-#play_game()
-
-
-board = {
-    'columns': 7,
-    'rows': 6,
-    'board': [
-        [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ']
-    ]
-}
 board1 = [
         [' ', ' ', ' ', ' ', ' ', ' ', ' ',
         ' ', ' ', ' ', ' ', ' ', ' ', ' ',
@@ -64,7 +41,12 @@ bb = {1: ' ', 2: ' ', 3: ' ', 4: ' ', 5: ' ', 6: ' ', 7: ' ',
     15: ' ', 16: ' ', 17: ' ', 18: ' ', 19: ' ', 20: ' ', 21: ' ',
     22: ' ', 23: ' ', 24: ' ', 25: ' ', 26: ' ', 27: ' ', 28: ' ',
     29: ' ', 30: ' ', 31: ' ', 32: ' ', 33: ' ', 34: ' ', 35: ' ',
-    36: ' ', 37: ' ', 38: ' ', 39: ' ', 40: ' ', 41: ' x', 42: ' '}
+    36: ' ', 37: ' ', 38: ' ', 39: ' ', 40: ' ', 41: ' ', 42: ' '}
+for key in bb:
+    if bb[key] == ' ':
+        bb[key] = '#'
+print(bb)
+#bb_n = {key: '#' if value == ' ' else value for key, value in bb.items()}
 
 #column list section
 columnDict = {
@@ -80,7 +62,234 @@ seven = [1,2,3,4,5,6,7]
 
 row1 = [36,37,38,39,40,41,42]
 
-pluto = board
+# Define a function that returns a dictionary with only the key-value pairs from a given dictionary where the value is equal to a given letter
+def dict_comp1(dictionary, letter):
+    return {key: value for key, value in dictionary.items() if value == letter}
+
+# Define a function that prints the values in a dictionary in a grid format
+def itg(dictionary,index2):
+    for row in range(6):
+        print('\n')
+        for col in range(7):
+            # Calculate the index of the current position in the dictionary
+            index = row*7 + col + 1
+            if index == index2:
+                print(Fore.CYAN, end ='')
+            # Print the value at the current position
+            print(dictionary[index], end = '||')
+            print(Fore.WHITE,end = '')
+def convertToBB(grid):
+    for row in range(6):
+        for col in range(7):
+            index = row*7 + col + 1
+            bb[index] = game.get_value(row,col)
+    return bb
+def create_number_list(start, end): 
+    number_list = []
+    for number in range(start, end+1):
+        number_list.append(number)
+    return number_list
+def printVictory(list):
+    for row in range(6):
+        print('\n')
+        for col in range(7):
+            # Calculate the index of the current position in the dictionary
+            index = row*7 + col + 1
+            if index in list:
+                print(Back.GREEN + bb[index], end ='||')
+            else:
+                print(bb[index], end = '||')
+                print(Fore.WHITE,end = '')
+                colorama.deinit()
+    print(Back.BLACK)
+def wrap_around_checker(list1):
+    print(list1)
+    leftEdge = [1, 8, 15, 22, 29, 36]
+    rightEdge = [7, 14, 21, 27, 35, 42]
+    is_wrap_around = False
+    if not all(x in leftEdge for x in list1) and not all(x in rightEdge for x in list1):
+        for num in list1[1:3]:
+            if num in leftEdge or num in rightEdge:
+                is_wrap_around = True
+                break
+        if is_wrap_around:
+            print('wrap around detected!!!!')
+
+            return False
+        else:
+            print(Back.CYAN)
+            print("here are the index numbers for the winning 4 tokens. No wrap around detected", list1)
+            printVictory(list1)
+            return True
+#this search function pinwheels around the played piece and checks if all 4 pieces are the same token
+#
+def searchL1(x, y, letter):
+    # Generate random 'X' and 'O' values for the positions on the game board
+    index2 = x*7 + y + 1
+    
+    grid = []
+    for row in range(6):
+        grid.append([])
+        for col in range(7):
+                index = row*7 + col + 1
+                value = bb.get(index)
+                # Store the value at the current position in the grid
+                grid[row].append(bb[index])  
+    # Create a dictionary of the indices that are multiples of 7
+    mo7 = {i: i for i in range(1, 43) if i % 7== 0}
+    values = {}
+    try:
+        # Populate the dictionary with the values of the surrounding positions
+        values = {
+        "tr": grid[x-1][y+1],
+        "tl": grid[x-1][y-1],
+        "ml": grid[x][y-1],
+        "mr": grid[x][y+1],
+        "bl": grid[x+1][y-1],
+        "bm": grid[x+1][y],
+        "br": grid[x+1][y+1]
+        }
+    except IndexError:
+        print('endie')
+    # Calculate the indices of the surrounding positions
+    tr = (7*(x-1)) + (y + 1) + 1
+    tl = (7*(x-1)) + (y-1) + 1
+    ml = (7*x) + (y-1) + 1
+    mr = (7*x) + (y+1) + 1
+    bl = (7*(x+1)) + (y-1) + 1
+    bm = (7*(x+1)) + y + 1
+    br = (7*(x+1)) + (y+1) + 1
+    
+    islam = {}
+    bud = {}
+    surroundings = [tr,tl,ml,mr,bl,bm,br]
+    posSur = [x for x in surroundings if x > 0 and x < 43]
+    print('p',posSur)
+    for items in posSur:
+        islam[items] = bb[items]
+        if bb[items] == letter:
+            bud[items] = bb[items]
+    
+    detector = dict_comp1(islam,letter)
+    # print('ddddd',detector)
+    # print("bud",bud)
+    # print(index2,"i")
+    result = []
+    result2 = []
+    result3 = []
+    result4 = []
+    for key, value in bud.items():
+        # key is the key of the dictionary (a string)
+        # value is the value of the dictionary (a number)
+        z = index2 - key
+        result.append(z)
+    firstIter = []
+    for item in bud.keys():
+        firstIter.append(item)
+    rate = []
+    for item in firstIter:
+        w = index2 - item
+        rate.append(w)
+        print(rate)
+    for i in range(len(rate)):
+        try:
+            nextLayer = index2 - (2*(rate[i]))
+            # print(f"nextLayer{nextLayer} = index2({index2}) + (2*rate:{rate[i]}[i{i}])" )
+            result2.append(nextLayer)
+        except NameError:
+            print('i')
+    for i in range(len(rate)):
+        try:
+            nextLayer2 = index2 - (3*(rate[i]))
+            # print(f"nextLayer{nextLayer2} = index2({index2}) + (2*rate:{rate[i]}[i{i}])" )
+            result3.append(nextLayer2)
+        except NameError:
+            print('i')
+    for i in range(len(rate)):
+        try:
+            nextLayer3 = index2 - (4*(rate[i]))
+            # print(f"nextLayer{nextLayer3} = index2({index2}) + (2*rate:{rate[i]}[i{i}])" )
+            result4.append(nextLayer3)
+        except NameError:
+            print('i')            
+    itg(bb,index2)
+    print(f"result2:{result2} and result3:{result3} and result:{result}")
+    pp = {}
+    ppp = {}
+    for items in result3:
+        if items == letter:
+            pp[items] = bb[items]
+    try:
+        for i, val  in enumerate(result2):
+            if bb[val] == letter:
+                pp[val] = bb[val]
+    except KeyError:
+        print('keyer')
+        pass
+    try:
+        for i, val  in enumerate(result3):
+            if bb[val] == letter:
+                ppp[val] = bb[val]
+    except KeyError:
+        print('keyerror')
+        pass
+    
+    thirdToken  = dict_comp1(pp,letter)
+    ct = 0
+    winlist = []
+    try:
+        for itm in ppp.keys():
+            if index2 < itm:
+                indexer = itm - index2
+                rater = indexer/3
+                if rater % 1 == 0:
+                    if bb[itm - rater] == letter:
+                        if len(winlist) > 0:
+                            winList2 = []
+                            winlist2.append(itm)
+                            winlist2.append(int(itm + rater))
+                            winlist2.append(int(itm + (2 * rater)))
+                            winlist2.append(index2)
+                            print("winList2:",winList2)
+                        winlist.append(itm)
+                        winlist.append(int(itm - rater))
+                        winlist.append(int(itm - (2 * rater)))
+                        winlist.append(index2)
+                        if wrap_around_checker(winlist):
+                            return True
+
+                        print("you won to the right motherfucker, this one subtracts the played tile from the final one that was found to be the same")
+                        print(f"itm:{itm}, indexer:{indexer}, played tile: {index2}, rater {rater}, winList{winlist}") 
+            else:
+                indexer = index2 - itm
+                rater = indexer/3
+                if rater % 1 == 0:
+                    if bb[itm + rater] == letter:
+                        if len(winlist) > 0:
+                            winList2 = []
+                            winlist2.append(itm)
+                            winlist2.append(int(itm + rater))
+                            winlist2.append(int(itm + (2 * rater)))
+                            winlist2.append(index2)
+                            print("winList2:",winList2)
+                        winlist.append(itm)
+                        winlist.append(int(itm + rater))
+                        winlist.append(int(itm + (2 * rater)))
+                        winlist.append(index2)
+                        if wrap_around_checker(winlist):
+                            return True
+                        print("you won to the left motherfucker, going down in index number")
+                        print(rater) 
+                        print(f"itm:{itm}, indexer:{indexer}, played tile: {index2}, rater {rater}, winList{winlist}") 
+      
+           
+        return winlist
+    except KeyError:
+        print('key')
+    except NameError:
+        pass
+    
+pluto = board1
 def whoFirst():
     who = int(input("Write the number 1 to go first and the number 2 if you want to go second\n"))
     if who != 1 and who != 2:
@@ -88,73 +297,44 @@ def whoFirst():
         if who != 1 and who != 2:
                 whoFirst()
     return who
-
-def add_7(numbers,i):
-    try:
-        h = numbers + 7
-    except TypeError:
-        whoFirst()
-    return h
-
-
-
-
+def whichLetter():
+    who = str(input("pick 'X' or 'O'"))
+    who.capitalize()
+    print(who)
+    if who != 'O' and who != 'X':
+        who = str(input("That wasn't one of the options asshole"))
+        if who != 'O' and who != 'X':
+            whoFirst()
+    return who
 def printBoard():
     for i,element in enumerate(pluto):
         print(pluto[i])
+
 def getRow():
     print('Pick the column that you would like to drop your piece into')
     a = int(input('Options (1-7) Enter here: '))
     try:
         assert a in range(1,8)
     except AssertionError:
-        getRow(a)
+        getRow()
     return a
-def gravity():
+def gravity(col,letter):
+    unfilled = 0
     #interate through each column, adding to a number that represents the level of the pieces
-    f = 7
-    a  = (getRow() - 1)
-    count = 0
-    l = 0
-    #iterate through the column, checking for how many spots are filled. get a number representing this
     for i in range(6):
-        b = game.get_value(i,a)
-        if b != '0':
-            count =+ 1
-
-        print("count:",count)
-        if count > 0:
-            if i == 5:
-                sum = (5 - count)
-                game.play_move(sum,a,'x')
-    game.print_board()
-
-
-
-
-
-
-
-
-                # do something with the column
-
-
-    #subject will later pick move by simply specifying which row they want to play in
-    #
-
-
-# Fore changes the text's foreground color
-#print(Fore.BLUE + "Blue Letters")
-
-#Back changes the text's background color
-#print(Back.WHITE + "White Background")
-
-def mark_space(num):
-    bb[num] = "X"
-
+        if game.get_value(i,col) == '#':
+            unfilled += 1
+    row3 = unfilled - 1
+    convertToBB(game)
+    game.play_move(row3,col,letter)
+    if (searchL1(row3,col,letter)):
+        print('you won!!!!! fucker')
+        exit()
+    else:
+        print('you did not win.... fucker')
 
  #return a value that represents the position the new piece will be added.
-def insertTokenFirstMove():
+def insertTokenFirstMove(letter):
     print("column number is the top number of  board in blue text.")
     pct("1   2   3   4   5   6   7", colorama.Fore.BLUE)
 
@@ -170,39 +350,41 @@ def insertTokenFirstMove():
         print(bb[e])
     except KeyError:
         print("that choice does not correspond to a location on the board")
-        insertTokenFirstMove()
-    #numbers = [i for i in range(1,43)]
-    # for colu in range(0,6):
-    #     for ro in range(0,7):
-    #         print(game.play_move(colu,ro))
-    game.play_move(5, (t-1),'X')
-
-
-
-    #print("yo" + andrea)
-    #amanda = bb[andrea].append("X |")
-  #  print(f"{t}:] ==" + amanda)
+        insertTokenFirstMove(letter)
+    game.play_move(5, (t-1),letter)
     if t in seven:
         game.print_board()
         #my_dict["Name"].append("Guru")
         #my_dict["Address"].append("Mumbai")
         #my_dict["Age"].append(bb30)
         #print(my_dict)
+def fcompMove(letter):
+    compcol = 0
+    x = (randint(1,7) - 1)
+    y = (randint(1,6) - 1)
+    gravity(x,letter)
 
-insertTokenFirstMove()
-gravity()
+def compMove(letter):
+    bestScore = -10000
 
 
-def compMove():
-    openSq = 0
-    empty_list = []
-    empty_list = list()
-    for i in range(5):
-        pass
-    pass
+
+
+def main():
+    player = whichLetter()
+    if player == 'X' or player == 'x':
+        computer = 'O'
+    else:
+        computer = 'X' 
+    insertTokenFirstMove(player)
+    win = 0 
+    while '#' in bb.values():
+        
+        fcompMove(computer)
+        game.print_board()
+        gravity(getRow()-1,player)
+        
+main()
 
 def minimax():
     pass
-
-def insertToken():
-    print("")
